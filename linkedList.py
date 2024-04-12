297915a32b92ee611e1e3e72cdbcbb62a979b268
Cameron Henning <cwh35@pitt.edu>
2024-04-12 14:12:35 -0400

    Added function to remove an element by specifying the value

--- linkedList.py
+++ linkedList.py
@@ -26,11 +26,13 @@ class LinkedList:
         
         iterator.next = Node(data, None)
 
+    # Inserting a list of values into a new linked list
     def insertValues(self, dataList):
         self.head = None
         for data in dataList:
             self.insertAtEnd(data)
 
+    # Insert a value after a specific value
     def insertAfterValue(self, dataAfter, dataToInsert):
         # If there is nothing in the list
         if self.head is None:
@@ -74,6 +76,25 @@ class LinkedList:
                 break
             iterator = iterator.next
             count += 1
+    
+    # Remove an element by value
+    def removeByValue(self, data):
+        if self.head is None:
+            return
+        
+        # Check if data is the head node
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        
+        iterator = self.head
+        while iterator.next:
+            if iterator.next.data == data:
+                iterator.next = iterator.next.next
+                break
+            iterator = iterator.next
+
+
 
     # Insert a value at a specific index
     def insertAt(self, index, data):
@@ -109,10 +130,20 @@ class LinkedList:
         print(linkedListString)
 
 if __name__ == '__main__':
+
     ll = LinkedList()
     ll.insertValues(["banana","mango","grapes","orange"])
     ll.print()
     ll.insertAfterValue("mango","apple") # insert apple after mango
+    ll.print()
+    ll.removeByValue("orange") # remove orange from linked list
+    ll.print()
+    ll.removeByValue("figs")
+    ll.print()
+    ll.removeByValue("banana")
+    ll.removeByValue("mango")
+    ll.removeByValue("apple")
+    ll.removeByValue("grapes")
     ll.print()
     # ll = LinkedList()
     # ll.insertAtBeginning(300)
