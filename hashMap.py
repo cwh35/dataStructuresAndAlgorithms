4d8598ec14e01618aa54fbf9adb0f9ea93485945
Cameron Henning <cwh35@pitt.edu>
2024-04-17 14:07:11 -0400

    Added test file for hashMap, removed main from hashMap.py

--- hashMap.py
+++ hashMap.py
@@ -64,25 +64,3 @@ class HashTable:
         for index, element in enumerate(self.arr[hash]):
             if element[0] == key:
                 del self.arr[hash][index]
-
-if __name__ == '__main__':
-    table = HashTable()
-    # h = table.getHash('march 6')
-    # print(h)
-    # table.add('april 15', 200)
-    # val = table.get('april 15')
-    # print(val)
-    # table['june 5'] = 920
-    # table['july 12'] = 45
-    # print(table['june 5'])
-    # print(table['july 12'])
-    # del table['june 5']
-    # print(table['june 5'])
-    # table.delete('july 12')
-    # print(table['july 12'])
-    table.add('march 6', 310)
-    table['march 17'] = 420
-    del table['march 6']
-    print(table['march 6'])
-    table.delete('march 17')
-    print(table['march 17'])
---
+++ test_hashMap.py
@@ -0,0 +1,30 @@
+from hashMap import HashTable
+
+def test_getHash():
+    table = HashTable()
+    assert table.getHash('hello') == 32
+    assert table.getHash('world') == 52
+
+def test_setItem():
+    # this also tests getItem
+    table = HashTable()
+    table['hello'] = 'world'
+    assert table['hello'] == 'world'
+
+def test_add():
+    # this also tests get()
+    table = HashTable()
+    table.add('hello', 'world')
+    assert table.get('hello') == 'world'
+
+def test_delItem():
+    table = HashTable()
+    table['hello'] = 'world'
+    del table['hello']
+    assert table.get('hello') == None
+
+def test_delete():
+    table = HashTable()
+    table.add('hello', 'world')
+    table.delete('hello')
+    assert table.get('hello') == None
